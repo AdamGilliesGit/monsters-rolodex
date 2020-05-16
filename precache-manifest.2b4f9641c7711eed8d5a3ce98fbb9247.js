e8e412c33b9f2186aa16ae97979bfcb66a692247
Adam Gillies <adam.d.gillies@gmail.com>
2020-05-16 18:22:16 +0100

    Updates

---
+++ precache-manifest.2b4f9641c7711eed8d5a3ce98fbb9247.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b698d1e6d4dab602c0403cfde19bffe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a5bd4aa08ee6ceab3caf",
+    "url": "/monsters-rolodex/static/css/main.a49fb09b.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5bd4aa08ee6ceab3caf",
+    "url": "/monsters-rolodex/static/js/main.ec9e0bf4.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
